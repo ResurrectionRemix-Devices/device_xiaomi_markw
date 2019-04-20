c77cb1a046c353322de9b86b7dd0302ccfc6c531
ZeeLog <Danjel.Zammit@gmail.com>
2019-04-21 01:04:58 +0200

    vndk: commonize folder & Update Graphics & Media stack from OnePlus3T

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,8 +1,46 @@
 ifeq ($(BOARD_VNDK_VERSION),)
 $(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
-LOCAL_PATH := $(call my-dir)
+VNDK_SP_LIBRARIES := \
+    android.hardware.graphics.allocator@2.0 \
+    android.hardware.graphics.mapper@2.0 \
+    android.hardware.graphics.mapper@2.1 \
+    android.hardware.graphics.common@1.0 \
+    android.hardware.graphics.common@1.1 \
+    android.hardware.renderscript@1.0 \
+    android.hidl.base@1.0 \
+    android.hidl.memory@1.0 \
+    libRSCpuRef \
+    libRSDriver \
+    libRS_internal \
+    libbacktrace \
+    libbase \
+    libbcinfo \
+    libblas \
+    libc++ \
+    libcompiler_rt \
+    libcutils \
+    libft2 \
+    libhardware \
+    libhidlbase \
+    libhidlmemory \
+    libhidltransport \
+    libhwbinder \
+    libion \
+    liblzma \
+    libpng \
+    libunwind \
+    libunwindstack \
+    libutils \
+    libutilscallstack \
+    libdexfile \
+	  libz
 
-include $(LOCAL_PATH)/vndk-sp-libs.mk
+EXTRA_VENDOR_LIBRARIES := \
+	android.hidl.manager@1.0
+#-------------------------------------------------------------------------------
+# VNDK Modules
+#-------------------------------------------------------------------------------
+LOCAL_PATH := $(call my-dir)
 
 vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
