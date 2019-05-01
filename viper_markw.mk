a8347bf1fad402a722664c249b327668b661ed14
ShihabZzz <shihab.riadn@gmail.com>
2019-05-01 14:21:30 +0600

    Update Fingerprint

--- viper_markw.mk
+++ viper_markw.mk
@@ -40,12 +40,8 @@ PRODUCT_MODEL := Redmi 4
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V9.6.2.0.MBECNFD release-keys" \
-    TARGET_DEVICE="markw" 
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.1.0.MBEMIXM release-keys" \
+    TARGET_DEVICE="markw"
 
-#Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.viper.maintainer=SonicBSV
-    
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V9.6.2.0.MBECNFD:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.1.0.MBEMIXM:user/release-keys
