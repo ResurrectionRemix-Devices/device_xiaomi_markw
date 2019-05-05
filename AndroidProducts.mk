39f3619daeff4510ecd1bea622e06c1cb6c2d4ed
ShihabZzz <shihab.riadn@gmail.com>
2019-05-05 12:55:16 +0600

    RR Pie Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/viper_markw.mk
+    $(LOCAL_DIR)/rr_markw.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo viper_markw-userdebug
+add_lunch_combo rr_markw-userdebug
